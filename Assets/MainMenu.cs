4b70cbf5fb65fe5f595c8f5f5197bfaabbe94d72
Iñaki Diez Galarza <inakidiezgalarza@gmail.com>
2020-10-13 05:04:06 -0300

    Agregada pantalla de carga + carga async

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -13,6 +13,7 @@ public class MainMenu : MonoBehaviour
     [SerializeField] GameObject help2Players = null;
     [SerializeField] float minTimeToLoad = 2.0f;
     [SerializeField] GameObject loadingScreen = null;
+    [SerializeField] Slider loadingBar = null;
 
     // Start is called before the first frame update
     void Start() {
@@ -22,11 +23,14 @@ public class MainMenu : MonoBehaviour
     public void PlayButton() {
         if (player1.isOn) {
             GameManager.SinglePlayer = true;
-            SceneManager.LoadScene("conduccion9");
+            StartCoroutine(PreloadScene("conduccion9"));
+            //SceneManager.LoadScene("conduccion9");
         }
         else if (player2.isOn) {
             GameManager.SinglePlayer = false;
-            SceneManager.LoadScene("conduccion9");
+            StartCoroutine(PreloadScene("conduccion9"));
+
+            //SceneManager.LoadScene("conduccion9");
         }
         else {
         }
@@ -49,28 +53,26 @@ public class MainMenu : MonoBehaviour
     {
         float loadingProgress;
         float timeLoading = 0;
-        yield return null;
         if (!SceneManager.GetSceneByName(scene).isLoaded)
         {
-            AsyncOperation ao = SceneManager.LoadSceneAsync(scene, LoadSceneMode.Additive);
+            AsyncOperation ao = SceneManager.LoadSceneAsync(scene);
             ao.allowSceneActivation = false;
+            loadingScreen.SetActive(true);
 
             while (!ao.isDone)
             {
                 timeLoading += Time.deltaTime;
                 loadingProgress = ao.progress + 0.1f;
                 loadingProgress = loadingProgress * timeLoading / minTimeToLoad;
-
+                loadingBar.value = loadingProgress;
                 // Loading completed
                 if (loadingProgress >= 1)
                 {
-                    loadingScreen.SetActive(false);
                     ao.allowSceneActivation = true;
 
                 }
                 yield return null;
             }
         }
-        SceneManager.SetActiveScene(SceneManager.GetSceneByBuildIndex(1));
     }
 }
